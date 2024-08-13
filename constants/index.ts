b4f49bdfcf25141109cf95fd8b53705901645c2f
Katlego Mokale <kdawgg28@gmail.com>
2024-08-13 23:16:58 +0200

    Constants Update

--- constants/index.ts
+++ constants/index.ts
@@ -25,3 +25,90 @@ export const sidebarLinks = [
       label: "Teachers",
     },
   ];
+
+  declare type Student = {
+    $id: string;
+    userId: string;
+    firstName: string;
+    SecondName: string;
+    Surname: string;
+    address1: string;
+    city: string;
+    Province: string;
+    postalCode: string;
+    dateOfBirth: string;
+    gender: string;
+    class: string;
+  };
+
+  declare type Teacher = {
+    $id: string;
+    userId: string;
+    firstName: string;
+    SecondName: string;
+    Surname: string;
+    address1: string;
+    city: string;
+    Province: string;
+    postalCode: string;
+    dateOfBirth: string;
+    gender: string;
+    class: string;
+  };
+
+  declare type Payment = {
+    $id: string;
+    userId: string;
+    PaymentDate: string;
+    Amount: string;
+    PaymentMethod: string;
+    PaymentRef: string;
+  };
+
+ declare type Parent = {
+    $id: string;
+    userId: string;
+    firstName: string;
+    SecondName: string;
+    Surname: string;
+    address1: string;
+    city: string;
+    Province: string;
+    postalCode: string;
+    dateOfBirth: string;
+    gender: string;
+    IdNumber: string;
+    Occupation: string;
+    ContactNumber: string;
+    Email: string;
+  };
+
+  declare type User = {
+    $id: string;
+    userId: string;
+    firstName: string;
+    SecondName: string;
+    Email: string;
+    password: string;
+    PhoneNumber: string;
+  };
+
+  declare type NewUserParams = {
+    userId: string;
+    email: string;
+    name: string;
+    password: string;
+  };
+
+  declare type Paymentypes =
+  | "Cash"
+  | "EFT"
+  | "Speed point ";
+
+  declare interface SiderbarProps {
+    user: User;
+  }
+
+  declare interface getStudentInfoProps {
+    userId: string;
+  }
