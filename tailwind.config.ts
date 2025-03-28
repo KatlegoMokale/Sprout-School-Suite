1e6730ef7fc4c872efd043d849529944af160d47
Katlego <kdawgg28@gmail.com>
2025-03-28 08:15:11 +0200

    Update dependencies and configuration for Tailwind CSS and PostCSS
    - Removed 'daycare-dashboard' from package.json and package-lock.json.
    - Added 'autoprefixer' as a dependency in both package files.
    - Updated 'postcss' version in package.json and package-lock.json.
    - Modified Tailwind CSS configuration to include additional file types for content scanning.
    - Deleted obsolete PostCSS configuration file.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,11 +3,10 @@ import type { Config } from "tailwindcss";
 const config = {
   darkMode: ["class"],
   content: [
-    "./pages/**/*.{ts,tsx}",
-    "./components/**/*.{ts,tsx}",
-    "./app/**/*.{ts,tsx}",
-    "./src/**/*.{ts,tsx}",
-    "./constants/**/*.{ts,tsx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./constants/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   prefix: "",
   theme: {
